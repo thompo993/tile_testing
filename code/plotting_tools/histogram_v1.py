42c8db7c4f01c163f51db92fd3e26437f2b20ddb
Ben Thompson <thompo993@gmail.com>
2025-09-04 11:27:15 +0100

    Updated File Path

--- code/plotting_tools/histogram_v1.py
+++ code/plotting_tools/histogram_v1.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Configuration - Edit these parameters as needed
-CSV_FILE_PATH = r"\\isis\shares\Detectors\Ben Thompson 2025-2026\Ben Thompson 2025-2025 Shared\Labs\Scintillating Tile Tests\pmt_rig_250825\spreadsheets\comparison_histogram__between_batches_csv_250903.csv" 
+CSV_FILE_PATH = r"\\isis\shares\Detectors\Ben Thompson 2025-2026\Ben Thompson 2025-2025 Shared\Labs\Scintillating Tile Tests\pmt_rig_250825\spreadsheets\Histograms\comparison_histogram__between_batches_csv_250904.csv" 
 BIN_NUMBER = 10  # Change this to adjust the number of bins
 ALPHA = 0.7  # Transparency for overlapping histograms (0-1)
 
